e5a714a1ee8c86faefc4639acd92ddbf4acfe437
Erick Rivas <49966187+ErickHR@users.noreply.github.com>
2020-01-23 06:02:45 -0600

    casitermi

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// 
+function addTemplate() {
+    let oneText = ['Past', 'Lively', 'Upcomming']
+    let twoText = ['Matches', 'Opposition', 'Matches']
+
+    let headerPerfil = document.querySelector('.header-perfil-detall-options-one')
+
+    let temp = document.querySelector('#selectsquare')
+
+    let div = temp.content.querySelectorAll('.square div')
+
+    for (i = 0; i < oneText.length; i++) {
+        div[0].textContent = oneText[i]
+        div[1].textContent = twoText[i]
+        headerPerfil.appendChild(document.importNode(temp.content, true))
+    }
+}
